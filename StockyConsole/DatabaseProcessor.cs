83437205cdeb276a4c0fe478c191e1d79ac495bc
Swapnil Naik GIT <ssnaik84@gmail.com>
2017-07-19 23:41:53 -0500

    working on candlestick patterns

--- StockyConsole/DatabaseProcessor.cs
+++ StockyConsole/DatabaseProcessor.cs
@@ -11,16 +11,16 @@ namespace StockyConsole
     {
         string connString = "Server=localhost;Database=nsedb;Uid=root;Pwd=dattagiri;";
 
-        public bool InsertEOD(DateTime DATE, string MKT,string SERIES, string SYMBOL,string SECURITY,
-                              double PREV_CL_PR,double OPEN_PRICE,double HIGH_PRICE,double LOW_PRICE,double CLOSE_PRICE,
-                              double NET_TRDVAL,double NET_TRDQTY,string IND_SEC,string CORP_IND,long TRADES,double HI_52_WK,double LO_52_WK)
+        public bool InsertEOD(DateTime DATE, string MKT, string SERIES, string SYMBOL, string SECURITY,
+                              double PREV_CL_PR, double OPEN_PRICE, double HIGH_PRICE, double LOW_PRICE, double CLOSE_PRICE,
+                              double NET_TRDVAL, double NET_TRDQTY, string IND_SEC, string CORP_IND, long TRADES, double HI_52_WK, double LO_52_WK)
         {
             string sqlCommand = "insert into EOD (DATE,MKT,SERIES,SYMBOL,SECURITY,PREV_CL_PR,OPEN_PRICE,HIGH_PRICE,LOW_PRICE,CLOSE_PRICE,NET_TRDVAL,NET_TRDQTY,IND_SEC,CORP_IND,TRADES,HI_52_WK,LO_52_WK)"
                        + " values (@DATE,@MKT,@SERIES,@SYMBOL,@SECURITY,@PREV_CL_PR,@OPEN_PRICE,@HIGH_PRICE,@LOW_PRICE,@CLOSE_PRICE,@NET_TRDVAL,@NET_TRDQTY,@IND_SEC,@CORP_IND,@TRADES,@HI_52_WK,@LO_52_WK)";
 
             MySqlConnection conn = new MySqlConnection(connString);
             try
-            {                
+            {
                 conn.Open();
                 MySqlCommand comm = conn.CreateCommand();
                 comm.CommandText = sqlCommand;
@@ -42,7 +42,7 @@ namespace StockyConsole
                 comm.Parameters.AddWithValue("@TRADES", TRADES);
                 comm.Parameters.AddWithValue("@HI_52_WK", HI_52_WK);
                 comm.Parameters.AddWithValue("@LO_52_WK", LO_52_WK);
-                
+
                 comm.ExecuteNonQuery();
             }
             catch (Exception ex)
@@ -51,11 +51,56 @@ namespace StockyConsole
             }
             finally
             {
-                if(conn != null && conn.State != System.Data.ConnectionState.Closed)
+                if (conn != null && conn.State != System.Data.ConnectionState.Closed)
                     conn.Close();
             }
             return true;
         }
 
+        internal List<string> InvertedHammerStocks(string latestDateString)
+        {
+            string sqlCommand = "select distinct eod0.SYMBOL from eod eod_2, eod eod_1, eod eod0, eod eod1, eod eod2 "
+                                + " where eod_2.SYMBOL = eod_1.SYMBOL and eod_1.SYMBOL = eod0.SYMBOL and eod0.SYMBOL = eod1.SYMBOL and eod1.SYMBOL = eod2.SYMBOL"
+                                + " and eod_2.DATE = '2017-07-13'"
+                                + " and eod_1.DATE = '2017-07-14'"
+                                + " and eod0.DATE = '2017-07-17'"
+                                + " and eod1.DATE = '2017-07-18'"
+                                + " and eod2.DATE = '2017-07-19'"
+                                + " and eod_2.CLOSE_PRICE < eod_2.OPEN_PRICE"  //negative
+                                + " and eod_1.CLOSE_PRICE < eod_1.OPEN_PRICE"  //negative
+                                + " and (((eod0.HIGH_PRICE - eod0.LOW_PRICE) > 3 * (eod0.OPEN_PRICE - eod0.CLOSE_PRICE)) AND((eod0.HIGH_PRICE - eod0.CLOSE_PRICE) / (.001 + eod0.HIGH_PRICE - eod0.LOW_PRICE) > 0.6) AND((eod0.HIGH_PRICE - eod0.OPEN_PRICE) / (.001 + eod0.HIGH_PRICE - eod0.LOW_PRICE) > 0.6))"
+                                //inverted hammer"
+                                + " and eod1.CLOSE_PRICE > eod1.OPEN_PRICE" // positive
+                                + " and eod2.CLOSE_PRICE > eod2.OPEN_PRICE"; // positive
+
+            MySqlConnection conn = new MySqlConnection(connString);
+            //MySqlDataReader reader = null;
+            List<string> result = null;
+            try
+            {
+                conn.Open();
+                MySqlCommand comm = conn.CreateCommand();
+                comm.CommandText = sqlCommand;
+
+                using (MySqlDataReader reader = comm.ExecuteReader())
+                {
+                    while (reader != null && reader.Read())
+                    {
+                        result.Add(Convert.ToString(reader["SYMBOL"]));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                if (conn != null && conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+            return result;
+
+        }
     }
 }
--- StockyConsole/Program.cs
+++ StockyConsole/Program.cs
@@ -7,46 +7,64 @@ using System.Threading.Tasks;
 namespace StockyConsole
 {
     class Program
-    {        
+    {
         static void Main(string[] args)
         {
+            //DateTime latestDate = new DateTime(2017, 7, 19);
+            //string latestDateString = latestDate.ToString("yyyy-MM-dd");
+            DownloadAndSaveDB();
+
+            //OpenInvertedHammer(latestDateString);
+
+
+
+
+            Console.ReadKey();
+        }
+
+        private static void OpenInvertedHammer(string latestDateString)
+        {
+            List<string> stockSymbols = new DatabaseProcessor().InvertedHammerStocks(latestDateString);
+
+
+        }
+
+        private static void DownloadAndSaveDB()
+        {
             var objDownloader = new BhavcopyDownloader();
-            DateTime startDate = new DateTime(2017, 7, 9); //(2015, 1, 1);
+            DateTime startDate = new DateTime(2017, 7, 18); //(2015, 1, 1);
             DateTime endDate = DateTime.Now;
             var allweekdays = new List<DateTime>();
             while (endDate >= startDate)
             {
                 startDate = startDate.AddDays(1);
 
-                if(startDate.DayOfWeek >= DayOfWeek.Monday && startDate.DayOfWeek <= DayOfWeek.Friday)
+                if (startDate.DayOfWeek >= DayOfWeek.Monday && startDate.DayOfWeek <= DayOfWeek.Friday)
                     allweekdays.Add(startDate);
             }
 
-            ////download
-            //foreach(var date in allweekdays)
-            //{
-            //    objDownloader.Download(date);
+            //download
+            foreach (var date in allweekdays)
+            {
+                objDownloader.Download(date);
 
-            //    //if (objDownloader.Download(date))
-            //    //    if (objDownloader.Unzip(date))
-            //    //        continue;
-            //}
+                //if (objDownloader.Download(date))
+                //    if (objDownloader.Unzip(date))
+                //        continue;
+            }
 
-            ////unzip
-            //foreach (var date in allweekdays)
-            //{
-            //    objDownloader.Unzip(date);
-            //}
+            //unzip
+            foreach (var date in allweekdays)
+            {
+                objDownloader.Unzip(date);
+            }
 
-            ////read file and insert data
-            //foreach (var date in allweekdays)
-            //{
-            //    objDownloader.ReadFileAndInsertData(date);
-            //}
-            
-
-
-            Console.ReadKey();
+            //read file and insert data
+            foreach (var date in allweekdays)
+            {
+                objDownloader.ReadFileAndInsertData(date);
+            }
         }
+
     }
 }
