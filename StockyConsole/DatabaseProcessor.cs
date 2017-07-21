d26c52d5e680253cd92a6d01f45c1b74103c4125
Swapnil Naik GIT <ssnaik84@gmail.com>
2017-07-20 23:24:22 -0500

    moved to SQL SERVER

--- StockyConsole/DatabaseProcessor.cs
+++ StockyConsole/DatabaseProcessor.cs
@@ -1,15 +1,16 @@
-﻿using MySql.Data.MySqlClient;
+﻿//using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace StockyConsole
 {
     public class DatabaseProcessor
     {
-        string connString = "Server=localhost;Database=nsedb;Uid=root;Pwd=dattagiri;";
+        string connString = "Server=.\\sqlexpress;Database=nsedb;Trusted_Connection=True;";    //mysql: "Server=localhost;Database=nsedb;Uid=root;Pwd=dattagiri;";
 
         public bool InsertEOD(DateTime DATE, string MKT, string SERIES, string SYMBOL, string SECURITY,
                               double PREV_CL_PR, double OPEN_PRICE, double HIGH_PRICE, double LOW_PRICE, double CLOSE_PRICE,
@@ -18,11 +19,13 @@ namespace StockyConsole
             string sqlCommand = "insert into EOD (DATE,MKT,SERIES,SYMBOL,SECURITY,PREV_CL_PR,OPEN_PRICE,HIGH_PRICE,LOW_PRICE,CLOSE_PRICE,NET_TRDVAL,NET_TRDQTY,IND_SEC,CORP_IND,TRADES,HI_52_WK,LO_52_WK)"
                        + " values (@DATE,@MKT,@SERIES,@SYMBOL,@SECURITY,@PREV_CL_PR,@OPEN_PRICE,@HIGH_PRICE,@LOW_PRICE,@CLOSE_PRICE,@NET_TRDVAL,@NET_TRDQTY,@IND_SEC,@CORP_IND,@TRADES,@HI_52_WK,@LO_52_WK)";
 
-            MySqlConnection conn = new MySqlConnection(connString);
+            //MySqlConnection conn = new MySqlConnection(connString);
+            SqlConnection conn = new SqlConnection(connString);
             try
             {
                 conn.Open();
-                MySqlCommand comm = conn.CreateCommand();
+                //MySqlCommand comm = conn.CreateCommand();
+                SqlCommand comm = conn.CreateCommand();
                 comm.CommandText = sqlCommand;
                 //"INSERT INTO room(person,address) VALUES(@person, @address)";
                 comm.Parameters.AddWithValue("@DATE", DATE);
@@ -57,15 +60,55 @@ namespace StockyConsole
             return true;
         }
 
-        internal List<string> InvertedHammerStocks(string latestDateString)
+        public List<string> GetLatestDates(int numberOfDates)
+        {
+            //mysql
+            //string sqlCommand = "select distinct date from eod order by date desc limit " + numberOfDates.ToString(); // descending
+            string sqlCommand = "select distinct top " + numberOfDates.ToString() + " date from eod order by date desc"; // descending
+
+            //MySqlConnection conn = new MySqlConnection(connString);
+            //MySqlDataReader reader = null;
+
+            SqlConnection conn = new SqlConnection(connString);
+            List<string> result = new List<string>();
+            try
+            {
+                conn.Open();
+                //MySqlCommand comm = conn.CreateCommand();
+                SqlCommand comm = conn.CreateCommand();
+                comm.CommandText = sqlCommand;
+
+                //using (MySqlDataReader reader = comm.ExecuteReader())
+                using (SqlDataReader reader = comm.ExecuteReader())
+                {
+                    while (reader != null && reader.Read())
+                    {
+                        result.Add(Convert.ToString(reader["DATE"]));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                if (conn != null && conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+            return result;
+
+        }
+
+        internal List<string> InvertedHammerStocks(string date_2, string date_1, string date0, string date1, string date2)
         {
             string sqlCommand = "select distinct eod0.SYMBOL from eod eod_2, eod eod_1, eod eod0, eod eod1, eod eod2 "
                                 + " where eod_2.SYMBOL = eod_1.SYMBOL and eod_1.SYMBOL = eod0.SYMBOL and eod0.SYMBOL = eod1.SYMBOL and eod1.SYMBOL = eod2.SYMBOL"
-                                + " and eod_2.DATE = '2017-07-13'"
-                                + " and eod_1.DATE = '2017-07-14'"
-                                + " and eod0.DATE = '2017-07-17'"
-                                + " and eod1.DATE = '2017-07-18'"
-                                + " and eod2.DATE = '2017-07-19'"
+                                + " and eod_2.DATE = '"+ date_2 +"'"
+                                + " and eod_1.DATE = '" + date_1 + "'"
+                                + " and eod0.DATE = '" + date0 + "'"
+                                + " and eod1.DATE = '" + date1 + "'"
+                                + " and eod2.DATE = '" + date2 + "'"
                                 + " and eod_2.CLOSE_PRICE < eod_2.OPEN_PRICE"  //negative
                                 + " and eod_1.CLOSE_PRICE < eod_1.OPEN_PRICE"  //negative
                                 + " and (((eod0.HIGH_PRICE - eod0.LOW_PRICE) > 3 * (eod0.OPEN_PRICE - eod0.CLOSE_PRICE)) AND((eod0.HIGH_PRICE - eod0.CLOSE_PRICE) / (.001 + eod0.HIGH_PRICE - eod0.LOW_PRICE) > 0.6) AND((eod0.HIGH_PRICE - eod0.OPEN_PRICE) / (.001 + eod0.HIGH_PRICE - eod0.LOW_PRICE) > 0.6))"
@@ -73,16 +116,20 @@ namespace StockyConsole
                                 + " and eod1.CLOSE_PRICE > eod1.OPEN_PRICE" // positive
                                 + " and eod2.CLOSE_PRICE > eod2.OPEN_PRICE"; // positive
 
-            MySqlConnection conn = new MySqlConnection(connString);
+            SqlConnection conn = new SqlConnection(connString);
+
+            // MySqlConnection conn = new MySqlConnection(connString);
             //MySqlDataReader reader = null;
-            List<string> result = null;
+            List<string> result = new List<string>();
             try
             {
                 conn.Open();
-                MySqlCommand comm = conn.CreateCommand();
+                //MySqlCommand comm = conn.CreateCommand();
+                SqlCommand comm = conn.CreateCommand();
                 comm.CommandText = sqlCommand;
 
-                using (MySqlDataReader reader = comm.ExecuteReader())
+                //using (MySqlDataReader reader = comm.ExecuteReader())
+                using (SqlDataReader reader = comm.ExecuteReader())
                 {
                     while (reader != null && reader.Read())
                     {
--- StockyConsole/Program.cs
+++ StockyConsole/Program.cs
@@ -10,11 +10,49 @@ namespace StockyConsole
     {
         static void Main(string[] args)
         {
-            //DateTime latestDate = new DateTime(2017, 7, 19);
-            //string latestDateString = latestDate.ToString("yyyy-MM-dd");
-            DownloadAndSaveDB();
+            DateTime startDate = new DateTime(2017, 7, 14);
+            DateTime endDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
 
-            //OpenInvertedHammer(latestDateString);
+            //get latest date
+            var latestDates = new DatabaseProcessor().GetLatestDates(1);
+            if (latestDates != null && latestDates.Count() > 0)
+            {
+                startDate = Convert.ToDateTime(latestDates[0]).AddDays(1);
+            }
+
+            var needToUpdateDB = true;
+
+            if(startDate > endDate)
+            {
+                Console.WriteLine("start date > end date, no need to update");
+                needToUpdateDB = false;
+            }            
+            
+            if(needToUpdateDB)
+                DownloadAndSaveDB(startDate, endDate);
+
+
+            //get latest 5 dates
+            latestDates = new DatabaseProcessor().GetLatestDates(5);
+
+            string date_2, date_1, date0, date1, date2;
+            if(latestDates.Count == 5)
+            {
+                date2 = latestDates[0];
+                date1 = latestDates[1];
+                date0 = latestDates[2];
+                date_1 = latestDates[3];
+                date_2 = latestDates[4];
+            }
+            else
+            {
+                Console.WriteLine("no last 5 days");
+                Console.ReadKey();
+                return;
+            }
+
+
+            OpenInvertedHammer(date_2, date_1, date0, date1, date2);
 
 
 
@@ -22,35 +60,40 @@ namespace StockyConsole
             Console.ReadKey();
         }
 
-        private static void OpenInvertedHammer(string latestDateString)
+        private static void OpenURLs(List<string> symbols)
         {
-            List<string> stockSymbols = new DatabaseProcessor().InvertedHammerStocks(latestDateString);
-
-
+            foreach (var symbol in symbols)
+            {
+                var url = "http://nseguide.com/charts.php?name=" + symbol;
+                System.Diagnostics.Process.Start(url);
+            }
         }
 
-        private static void DownloadAndSaveDB()
+        private static void OpenInvertedHammer(string date_2, string date_1, string date0, string date1, string date2)
+        {
+            List<string> stockSymbols = new DatabaseProcessor().InvertedHammerStocks(date_2, date_1, date0, date1, date2);
+
+            //open urls in browsers
+            OpenURLs(stockSymbols);
+        }
+
+        private static void DownloadAndSaveDB(DateTime startDate, DateTime endDate)
         {
             var objDownloader = new BhavcopyDownloader();
-            DateTime startDate = new DateTime(2017, 7, 18); //(2015, 1, 1);
-            DateTime endDate = DateTime.Now;
+            
             var allweekdays = new List<DateTime>();
             while (endDate >= startDate)
             {
-                startDate = startDate.AddDays(1);
-
                 if (startDate.DayOfWeek >= DayOfWeek.Monday && startDate.DayOfWeek <= DayOfWeek.Friday)
                     allweekdays.Add(startDate);
+
+                startDate = startDate.AddDays(1);
             }
 
             //download
             foreach (var date in allweekdays)
             {
                 objDownloader.Download(date);
-
-                //if (objDownloader.Download(date))
-                //    if (objDownloader.Unzip(date))
-                //        continue;
             }
 
             //unzip
